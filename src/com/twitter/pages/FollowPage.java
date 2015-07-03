bc91fa144e95de36b21a1794c68ba47116002f88
unknown <illreceivemail@gmail.com>
2015-07-03 12:18:27 +0300

    FollowPage: add Methods with comments: convertStringToDate, parseStringToProperDateFormat. change logic for method isProperDate

--- src/com/twitter/pages/FollowPage.java
+++ src/com/twitter/pages/FollowPage.java
@@ -7,8 +7,11 @@ import com.twitter.utils.Reporter;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created
@@ -23,6 +26,7 @@ public class FollowPage extends BasePage {
     private By tweetDateLocator = By.xpath(".//div[contains(@class,'stream-item-header')]/small/a");
     private By reTweetCount = By.xpath(".//div[contains(@class,'ProfileTweet-action ProfileTweet-action--retweet js-toggleState js-toggleRt')]/button[2]/span[3]/span");
     private By followLink = By.xpath("//a[contains(@class,'ProfileHeaderCard-screennameLink u-linkComplex js-nav')]");
+    String dateAttr="data-original-title";
 
     /**
      * getter for locator of all tweets on page
@@ -63,9 +67,10 @@ public class FollowPage extends BasePage {
      * @param date - all tweet before this date will be returned in new collection
      * @return - List<WebElement> - collection of tweets with proper date
      */
-    public List<WebElement> getListOfTweetsWithProperDate( List<WebElement> elements, LocalDate date) {
+    public List<WebElement> getListOfTweetsWithProperDate( List<WebElement> elements, LocalDate date,String localeString) {
         Reporter.log("get list of tweets with proper date");
         String tweetDateString;
+        String fullTweetDateTime;
         List<WebElement> properDateList = new ArrayList<>();
         for (int i = 0; i < elements.size(); i++) {
             WebElement element = elements.get(i);
@@ -73,10 +78,20 @@ public class FollowPage extends BasePage {
             WebElement elDate = element.findElement(tweetDateLocator);
             //Reporter.log("try to find retweet button");
             tweetDateString = elDate.getText();
-            String[] strArr = tweetDateString.split(" ");
+            fullTweetDateTime = elDate.getAttribute(dateAttr);
+            Reporter.log("date from tweet ATTRIBUTE: " + fullTweetDateTime);
+            //String[] strArr = tweetDateString.split(" ");
+
+            //
+            String tweetDateParsed = parseStringToProperDateFormat(tweetDateString);
+            LocalDate tweetDate = convertStringToDate(tweetDateParsed, localeString);
+            //
+
             Reporter.log("date from tweet: " + tweetDateString);
-            if (isProperDate(date, strArr)) {
-                properDateList.add(element);
+            if (tweetDate != null) {
+                if (isProperDate(date, tweetDate)) {
+                    properDateList.add(element);
+                }
             }
         }
         return properDateList;
@@ -100,12 +115,29 @@ public class FollowPage extends BasePage {
 
     /**
      * compare tweet date and date
+     * @param currentDate - LocalDate - used to compare with tweet date
+     * @param tweetDate - LocalDate - date from tweet
+     * @return  - true - if tweet date is before the date, false - if tweet date is after date or same
+     */
+    private boolean isProperDate(LocalDate currentDate, LocalDate tweetDate) {
+
+        if ((tweetDate.getMonth().getValue() == currentDate.getMonth().getValue() && tweetDate.getDayOfMonth() < currentDate.getDayOfMonth()) ||
+                tweetDate.getMonth().getValue() <= currentDate.getMonth().getValue())
+            return true;
+
+        return false;
+    }
+
+    /**
+     * compare tweet date and date
      * @param date - LocalDate - used to compare with tweet date
-     * @param tweetDate - String - date from tweet
+     * @param tweetDateString - String - date from tweet
      * @return - true - if tweet date is before the date, false - if tweet date is after date or same
      */
-    private boolean isProperDate(LocalDate date, String[] tweetDate) {
+    private boolean isProperDate(LocalDate date, String tweetDateString) {
         //Reporter.log("check is tweetDateString of tweet proper for retweet");
+        String[] tweetDate = tweetDateString.split(" ");
+
         Month month = null;
         int day = Integer.parseInt(tweetDate[0]);
 
@@ -143,6 +175,44 @@ public class FollowPage extends BasePage {
             return true;
 
         return false;
+    }
+
+
+    /**
+     * parse in string to proper date format string
+     * @param inDateString - String to parse
+     * @return - String - in proper format
+     */
+    private static String parseStringToProperDateFormat(String inDateString) {
+        String rez = "";
+        String year = "0001";
+        String str = inDateString.replace(".", "");
+        str = str.replace("de ", "");
+        String[] strArr = str.split(" ");
+        if (strArr[0].matches("[0-9]+")) {
+            rez += strArr[0] + " " + strArr[1].substring(0, 3) + ", " + year;
+        } else {
+            rez += strArr[1] + " " + strArr[0].substring(0, 3) + ", " + year;
+        }
+        return rez;
+    }
+
+    /**
+     * convert string to date with DateTimeFormatter
+     * @param dateIn - String - contains string to convert
+     * @param locale - String - locale of the date
+     * @return - LocalDate - contains date converted from string
+     */
+    public LocalDate convertStringToDate(String dateIn, String locale) {
+        Locale local = Locale.forLanguageTag(locale);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM, yyyy", local);
+        LocalDate date;
+        try {
+            date = LocalDate.parse(dateIn, formatter);
+        } catch (DateTimeParseException ex) {
+            date = null;
+        }
+        return date;
     }
 
     /**
